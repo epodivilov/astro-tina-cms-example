713da9c5ea53fbe4dfe68dc651aff9964aecbcac
Evgeniy Podivilov <evgeniy.podivilov@gmail.com>
2025-05-29 15:39:49 +0100

    chore(config): 🔧 Update site and base environment variables in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-import react from '@astrojs/react';
-import tinaDirective from "./astro-tina-directive/register"
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import react from "@astrojs/react";
+import tinaDirective from "./astro-tina-directive/register";
 
 // https://astro.build/config
 export default defineConfig({
-	site: process.env.SITE_URL || `https://${process.env.VERCEL_URL}`,
-	integrations: [mdx(), sitemap(), react(), tinaDirective()],
+  site: process.env.ASTRO_SITE,
+  base: process.env.ASTRO_BASE,
+  integrations: [mdx(), sitemap(), react(), tinaDirective()],
 });
